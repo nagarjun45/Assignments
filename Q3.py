bf9fa733e6257b36a08a580afb9385d15841f76b
nagarjun45 <nagarjun.repala@datagrokr.com>
2019-11-15 09:41:12 +0530

    all files added to the repository

---
+++ Q3.py
@@ -0,0 +1,4 @@
+lis= [12,24,35,24,88,120,155,88,120,155]
+
+newList = sorted(list(set(lis)))
+print(newList)
---
+++ test.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="arjun123",
+  database="sakila"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("create table customers5 (id int,name varchar(20),address varchar(20))")
+
+sql = "INSERT INTO customers5 (id,name, address) VALUES (%s,%s, %s)"
+val = (5,"John", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
